3d6da63273f7f1cb5d0f4a7025b4a8235a87092d
Randy Charlemagne <randy.charlemagne.pro@gmail.com>
2025-02-26 11:02:21 +0100

    Ajout de la class soigneur

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ public class Main {
         Arme arc = new Arme("Arc en bois", 5);
         Arme epee = new Arme("Epee en bois", 5);
         Arme baguette = new Arme("Baguette en bois ancien", 8);
+        Arme sceptre = new Arme("Baguette en fer", 20);
 
         Personnage mage = new Mage("Randy", baguette);
         Personnage guerrier = new Guerrier("Wayl", epee);
-        Personnage archer = new Archer("Théo le noob", arc);
+        Personnage archer = new Archer("Archer", arc);
+        Personnage soigneur = new Soigneur("Pierre",sceptre);
 
         mage.typeAttaque.attaque(mage, archer);
+        System.out.println(archer.getPointVie());
         mage.typeAttaque.attaque(mage, archer);
         System.out.println(archer.getPointVie());
+
+        soigneur.typeAttaque.attaque(soigneur, archer);
+        System.out.println(archer.getPointVie());
+
 
     }
 }
